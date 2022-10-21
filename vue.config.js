ae98bcbdd65cba6b4ffada2bf6fa4c9ac080d88d
truman feng <truman.feng@deltafaucet.com>
2022-10-21 14:55:20 +0800

    chaging url

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         port: 9876,
         proxy: {                 //设置代理，必须填
             '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
-                target: 'http://localhost:8081',     //代理的目标地址
+                target: 'https://graduation-backstage-truman.azurewebsites.net:8081',     //代理的目标地址
                 changeOrigin: true,              //是否设置同源，输入是的
                 ws: true,
                 pathRewrite: {                   //路径重写
